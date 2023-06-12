70eb4bbf5aa474a8480463e61992a9cb4278080a
Alexandr <sanya2oo11111@gmail.com>
2023-06-13 01:40:28 +0300

    change: генерация ссылко на изображения

--- js/script.js
+++ js/script.js
@@ -2,22 +2,26 @@ const data = [
     html = {
         name: 'html',
         width: 40,
-        class: 'skill-item_html'
+        class: 'skill-item_html',
+        path: 'html.svg'
     },
     css = {
         name: 'css',
         width: 40,
-        class: 'skill-item_css'
+        class: 'skill-item_css',
+        path: 'css.svg'
     },
     pyhton = {
         name: 'python',
         width: 60,
-        class: 'skill-item_python'
+        class: 'skill-item_python',
+        path: 'python.svg'
     },
     cpp = {
         name: 'c++',
         width: 80,
-        class: 'skill-item_cpp'
+        class: 'skill-item_cpp',
+        path: 'c++.svg'
     }
 ]
 
@@ -25,8 +29,9 @@ const skillList = document.querySelector('dl.skill-list')
 
 data.forEach(element => {
     const dt = document.createElement('dt')
-    dt.classList.add('skill-item', element.class)
+    dt.classList.add('skill-item')
     dt.innerHTML = element.name;
+    dt.style.backgroundImage = `url(../img/skills/${element.path})`;
     skillList.append(dt)
     const dd = document.createElement('dd')
     const div = document.createElement('div')
