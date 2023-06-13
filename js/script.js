baf6b88454f5d116756bcdae56482e7c9dadfe8d
Alexandr <72127302+Repeyn1k@users.noreply.github.com>
2023-06-13 14:40:15 +0300

    fix: замечания

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 const skills = {
     data: [
-        html = {
+        {
             name: 'html',
             width: 40,
             path: 'html.svg'
         },
-        css = {
+        {
             name: 'css',
             width: 40,
             path: 'css.svg'
         },
-        pyhton = {
+        {
             name: 'python',
             width: 60,
             path: 'python.svg'
         },
-        cpp = {
+        {
             name: 'c++',
             width: 80,
             path: 'c++.svg'
         }
     ],
-    generateList: function(parrent) {
+    generateList: function(parent) {
         this.data.forEach(element => {
             const dt = document.createElement('dt')
             dt.classList.add('skill-item')
             dt.innerHTML = element.name;
             dt.style.backgroundImage = `url(img/skills/${element.path})`;
-            parrent.append(dt)
+            parent.append(dt)
             const dd = document.createElement('dd')
             const div = document.createElement('div')
             div.style.width = `${element.width}%`;
+            div.innerHTML = `${element.width}%`;
             dd.classList.add('skill-level')
             dd.append(div)
-            parrent.append(dd)
+            parent.append(dd)
         });
     }
 }
